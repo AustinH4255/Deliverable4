3b0a6352e665fb1f606c0e04a22076ec1d56091c
autin <autin@LAPTOP-0UU2O9UK>
2023-02-08 09:14:50 -0500

    Add project files.

---
+++ Deliverable4/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Author: Austin Hall
+ * Date: 2/8/2023
+ * Description: A program made to show the Fibonacci sequence 25 times.
+ */
+
+using System;
+
+namespace Deliverable4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f1 = 0;
+            int f2 = 1;
+            int f3 = 0;
+
+            for (int i = 0; i < 25; i++)
+            {
+
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+
+                if (i == 0)
+                {
+                    f1 = 0;
+                }
+
+                Console.WriteLine("Fibonacci Number: " + f1.ToString());
+            }
+        }
+    }
+}
